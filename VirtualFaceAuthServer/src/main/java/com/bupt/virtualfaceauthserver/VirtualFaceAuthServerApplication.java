d661e4fca6ee57f28efff6b0c7a42f0a6fb1b2eb
wyimaomao <2822986820@qq.com>
2023-08-06 20:49:49 +0800

    Initialization

---
+++ VirtualFaceAuthServer/src/main/java/com/bupt/virtualfaceauthserver/VirtualFaceAuthServerApplication.java
@@ -0,0 +1,13 @@
+package com.bupt.virtualfaceauthserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualFaceAuthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualFaceAuthServerApplication.class, args);
+	}
+
+}
---
+++ VirtualFaceAuthServer/src/test/java/com/bupt/virtualfaceauthserver/VirtualFaceAuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bupt.virtualfaceauthserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualFaceAuthServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
